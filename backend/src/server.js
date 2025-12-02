443cb426bc24ad8d77ff63c0e0011b2e792a6574
ys <965483569@qq.com>
2025-12-02 11:29:57 +0800

    准备部署

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
+
+// 生产环境部署的配置 
+if(process.env.NODE_ENV === 'production') {
+  // 静态资源服务
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  
+  // 处理所有未匹配的路由，返回前端应用的入口文件
+  app.get('*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
