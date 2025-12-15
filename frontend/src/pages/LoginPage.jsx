49c82652dc4937af4e19c796c4854c80765a1a68
ys <965483569@qq.com>
2025-12-15 08:18:37 +0800

    前端代码块设置

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold text-white'>Login Page</h1>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUpPage() {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold text-white'>Sign Up Page</h1>
+    </div>
+  )
+}
+
+export default SignUpPage
